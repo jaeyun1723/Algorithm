0ab210a8d9556fcf136526420934c17f2e05fa71
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-06-19 11:56:36 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2880 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<map>
+#include<deque>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	priority_queue <int, vector<int>, greater<int> > pq;
+	int N;
+	cin >> N;
+	while (N--)
+	{
+		int num;
+		cin >> num;
+		if (num == 0)
+		{
+			if (pq.empty())
+				cout << 0 << '\n';
+			else
+			{
+				cout <<  pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+			pq.push(num);
+	}
+}
