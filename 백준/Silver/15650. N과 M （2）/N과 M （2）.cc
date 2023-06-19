6758a2919fc48dbd5b6ee9d5f1cb55dae562e34a
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-06-19 12:12:41 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<map>
+#include<deque>
+#include<list>
+using namespace std;
+
+int N, slt;
+vector<int> v;
+vector<bool> visited;
+
+void permutation(int n, int idx) {
+	if (n == slt) {
+		for (auto it : v)
+			cout << it << " ";
+		cout << '\n';
+		return;
+	}
+	for (int i = idx; i <= N; i++) {
+		if (!visited[i]) {
+			v.push_back(i);
+			visited[i] = true;
+			permutation(n + 1, i + 1);
+			visited[i] = false;
+			v.pop_back();
+		}
+	}
+}
+
+int main() {
+	cin >> N >> slt;
+	visited.resize(N + 1, false);
+	permutation(0, 1);
+}
