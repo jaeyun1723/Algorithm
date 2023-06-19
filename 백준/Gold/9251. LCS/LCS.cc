62074e415b14df28c49bfe3df6895179f6545904
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-06-19 12:07:55 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<map>
+#include<deque>
+#include<bitset>
+using namespace std;
+
+typedef long long ll;
+
+string str1, str2;
+int dp[1001][1001];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> str1 >> str2;
+	for (int i = 1; i <= str1.size(); i++) {
+		for (int j = 1; j <= str2.size(); j++) {
+			if (str1[i - 1] == str2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+			else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	cout << dp[str1.size()][str2.size()] << '\n';
+}
