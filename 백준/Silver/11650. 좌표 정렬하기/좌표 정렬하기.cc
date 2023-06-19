3cfad2048b516bc1fbc85c4d0b63200d92d99542
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-06-19 12:11:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 124 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+
+bool compare(pair<int, int> x, pair<int, int> y)
+{
+	if (x.first == y.first)
+	{
+		return (x.second < y.second);
+	}
+	return x.first < y.first;
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector< pair<int, int> > v;
+	while (N--)
+	{
+		int x, y;
+		cin >> x >> y;
+		v.push_back(make_pair(x, y));
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i].first << " " << v[i].second << '\n';
+}
