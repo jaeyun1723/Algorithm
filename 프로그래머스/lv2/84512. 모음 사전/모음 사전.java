2a9a177cc31824045a1772e25086927da7517157
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-07-20 09:09:56 +0900

    [level 2] Title: 모음 사전, Time: 2.50 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,21 +1,20 @@
 class Solution {
     static String[] vowel={"A","E","I","O","U"};
+    static int answer=0, cnt=0;
     static String target;
-    static int answer = -1, cnt=0;
-    static void dictionary(String word)
-    {
-        if(target.equals(word)) {
-            answer=cnt;
-            return;
-        }
-        if(word.length()==5) return;
+    
+    static void dictionary(String word){
+      if(target.equals(word)){
+          answer=cnt;
+          return;
+      }
+      if(word.length()>=5||answer!=0)
+          return;
         for(int i=0;i<vowel.length;i++){
             cnt++;
             dictionary(word+vowel[i]);
         }
-
     }
-    
     public int solution(String word) {
         target=word;
         dictionary("");
