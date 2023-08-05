365977b29f62057deff298e03a976a957a2b6e49
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-08-05 13:12:58 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 98 ms, Memory: 18,652 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Solution {
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+        int T = Integer.parseInt(st.nextToken());
+ 
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            char prev = str.charAt(0);
+            int cnt = prev == '1' ? 1 : 0;
+            for (int i = 1; i < str.length(); i++) {
+                if (str.charAt(i) != prev)
+                    cnt++;
+                prev = str.charAt(i);
+            }
+        System.out.println("#" + t + " " + cnt);
+        }
+ 
+    }//
+ 
+}//
