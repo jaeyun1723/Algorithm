1a98efe77e6ca7b61baa5dc9fa2162d5f425e032
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-08-05 13:08:28 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,57 +1,42 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/*
+ *  신맛 : 곱 , 쓴맛 : 합
+ */
 public class Main {
 
-	static int N, diff = 100000001;
-	static int[] S, B;
+	static int N, diff;
+	static int[][] ingredient;
 	static boolean[] isSelected;
+
+	static void cook(int idx, int sour, int bitter, int cnt) {
+		diff = Math.min(diff, (cnt == 0) ? Integer.MAX_VALUE : Math.abs(sour - bitter));
+		if (idx == N)
+			return;
+		cook(idx + 1, sour * ingredient[idx][0], bitter + ingredient[idx][1], cnt + 1);
+		cook(idx + 1, sour, bitter, cnt);
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
-		S = new int[N];
-		B = new int[N];
-		isSelected=new boolean[N];
+		ingredient = new int[N][2];
+		isSelected = new boolean[N];
+		diff = Integer.MAX_VALUE;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			S[i] = Integer.parseInt(st.nextToken());
-			B[i] = Integer.parseInt(st.nextToken());
+			int sour = Integer.parseInt(st.nextToken());
+			int bitter = Integer.parseInt(st.nextToken());
+			ingredient[i][0] = sour;
+			ingredient[i][1] = bitter;
 		}
-		Cook(0);
+		cook(0, 1, 0, 0);
 		System.out.println(diff);
-	}//
-
-	static void Cook(int cnt) {
-		if (cnt == N) {
-			int sumS = 1;
-			int sumB = 0;
-			boolean flag = false;
-			for (int i = 0; i < N; i++) {
-				if (isSelected[i]) {
-					sumS *= S[i];
-					sumB += B[i];
-					flag = true;
-				}
-			}
-			if (flag) {
-				int result = Math.abs(sumS - sumB);
-				if (result < diff)
-					diff = result;
-			}
-			return;
-		}
-		isSelected[cnt] = true;
-		Cook(cnt + 1);
-		isSelected[cnt] = false;
-		Cook(cnt + 1);
 	}
 
 }
-//
