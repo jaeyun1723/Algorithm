1d8fed76a311f7a4a7de38be89e257003eb8b4ce
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-08-05 13:14:26 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 101 ms, Memory: 17,688 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		for (int t = 1; t <= 10; t++) {
+			st = new StringTokenizer(br.readLine());
+			int dump = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+			int[] arr = new int[101];
+			int start = 101;
+			int end = -1;
+			for (int i = 0; i < 100; i++) {
+				int num = Integer.parseInt(st.nextToken());
+				start = Math.min(start, num);
+				end = Math.max(end, num);
+				arr[num]++;
+			}
+
+			while (dump-- > 0) {
+				arr[start]--;
+				arr[end]--;
+				arr[end-1]++;
+				arr[start + 1]++;
+				if (arr[start] == 0)
+					while (arr[start] == 0)
+						start++;
+				if (arr[end] == 0)
+					while (arr[end] == 0)
+						end--;
+				if (start >= end)
+					break;
+			}
+			
+			System.out.println("#" + t + " " + (end - start));
+		}
+
+	}//
+
+}//
