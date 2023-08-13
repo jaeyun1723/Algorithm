d23f9b64e4b0ae6c3957bdf2adab6c8678bc9a9d
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-08-13 14:44:41 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,56 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class Key {
-
-        int x;
-        int y;
-
-        public Key(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if(this == obj) return true;
-            if(obj == null || getClass()!=obj.getClass() ) return false;
-
-            Key key=(Key) obj;
-            return x==key.x && y==key.y;
-        }
-        @Override
-        public int hashCode() {
-            return Objects.hash(x,y);
-        }
-
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        Map<Key, Integer> map = new HashMap<>();
+        boolean[][] arr = new boolean[101][101];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
-                    Key key = new Key(j, k);
-                    map.put(key, 1);
+                    arr[j][k] = true;
                 }
             }
         }
-        System.out.println(map.size());
-
+        int area=0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[i][j]) area++;
+            }
+        }
+        System.out.println(area);
     }//
 }//
