f9a396c3ab885c04817df9b92abfb6e6baea01c3
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-09-03 16:55:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer=1;
+        while(Math.abs(a-b)>1||Math.min(a,b)%2==0){
+            a=(a+1)/2;
+            b=(b+1)/2;
+            answer++;
+            
+        }
+        return answer;
+    }
+}
