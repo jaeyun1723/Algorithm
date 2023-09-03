b579ac39fa044d4c860185db51407ca125a89cca
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-09-03 16:35:51 +0900

    [level 2] Title: 구명보트, Time: 8.71 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
+       int answer=0, start=0,end=people.length-1;
         Arrays.sort(people);
-        int start=0,end=people.length-1;
         while(start<=end){
-            if(people[start]+people[end]<=limit)
-            {
-                start++; end--;
+            if(people[start]+people[end]<=limit){
+                start++;
+                end--;
             }
             else
                 end--;
