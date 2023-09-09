2b177b3b2f700cf89e91df43ab98602376241e44
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-09-09 14:46:06 +0900

    [unrated] Title: 귤 고르기, Time: 51.78 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
+        int answer=0;
+        Map<Integer,Integer> ggul=new HashMap<>();
+        for(int i=0;i<tangerine.length;i++){
+            ggul.put(tangerine[i],ggul.getOrDefault(tangerine[i],0)+1);
+        }
+         List<Integer> list=new ArrayList<>(ggul.keySet());
+        Collections.sort(list,(o1,o2)->ggul.get(o2)-ggul.get(o1));
+        
         int sum=0;
-        int[] cnt = new int[10000001];
-        for(int i=0;i<tangerine.length;i++)
-            cnt[tangerine[i]]++;
-        Arrays.sort(cnt);
-        for(int i=cnt.length-1;i>=0;i--){
-            if(cnt[i]==0)continue;
-            if(sum<k) {
-                sum+=cnt[i];
-                answer++;
-            }
-            else return answer;
-            }
+        for(int i=0;i<list.size();i++){
+            sum+=ggul.get(list.get(i));
+            answer++;
+            if(sum>=k) break;
+        }
+        
         return answer;
     }
 }
