812d43426f365f5683772af8c25b543488bb3aa1
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-04-27 19:06:05 +0900

    [level 2] Title: H-Index, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer=0;
+        for(int i=citations.length;i>=0;i--){
+            int cnt=0;
+            for(int j=0;j<citations.length;j++){
+                if(citations[j]>=i) cnt++;
+            }
+            if(cnt>=i){
+                return i;
+            }
+        }
+        
+        return answer;
+    }
+}
