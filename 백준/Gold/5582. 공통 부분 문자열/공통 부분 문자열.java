b748423d07d9b261e0783d291d83f19e14144122
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-08-05 15:25:32 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 236 ms, Memory: 77532 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String s1 = st.nextToken();
+        st = new StringTokenizer(br.readLine());
+        String s2 = st.nextToken();
+
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        int max = 0;
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+
+
+}
