720170cf9fb593a77ee5399f44c1d92067e0f78b
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-08-05 15:17:27 +0900

    [level 3] Title: 단속카메라, Time: 6.68 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+class Solution {
+
+    public int solution(int[][] routes) {
+        int answer = 1;
+
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
+        int cam = routes[0][1];
+
+        for (int i = 1; i < routes.length; i++) {
+            if (cam < routes[i][0]) {
+                answer++;
+                cam = routes[i][1];
+            }
+        }
+
+        return answer;
+    }
+}
