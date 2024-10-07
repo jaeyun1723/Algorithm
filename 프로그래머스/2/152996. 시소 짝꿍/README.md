5fbd2761969d5f77f8dedf0c71a713d04bed3eb5
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-10-07 09:14:28 +0900

    [level 2] Title: 시소 짝꿍, Time: 80.58 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/README.md
+++ 프로그래머스/2/152996. 시소 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 시소 짝꿍 - 152996 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996?language=java) 
 
 ### 성능 요약
 
-메모리: 116 MB, 시간: 118.46 ms
+메모리: 104 MB, 시간: 80.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 17:17:54
+2024년 10월 07일 09:14:17
 
 ### 문제 설명
 
--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -4,31 +4,33 @@ class Solution {
 
     public long solution(int[] weights) {
         long answer = 0;
-
+        
         Arrays.sort(weights);
-
-        Map<Double, Integer> map = new HashMap<>();
-        for (int w : weights) {
-            // 2/3 2/4 3/4
-            double wa = w * 1.0;
-            double wb = w * 2 / 3.0;
-            double wc = w / 2.0;
-            double wd = w * 3 / 4.0;
-            if (map.containsKey(wa)) {
-                answer += map.get(wa);
+        
+        Map<Double, Integer> map=new HashMap<>();
+        
+        for(int w : weights){
+            
+            double w1=w*1.0;
+            double w2=w/2.0;
+            double w3=w*2/3.0;
+            double w4=w*3/4.0;
+            
+            if(map.containsKey(w1)){
+                answer+=map.get(w1);
             }
-            if (map.containsKey(wb)) {
-                answer += map.get(wb);
+            if(map.containsKey(w2)){
+                answer+=map.get(w2);
             }
-            if (map.containsKey(wc)) {
-                answer += map.get(wc);
+            if(map.containsKey(w3)){
+                answer+=map.get(w3);
             }
-            if (map.containsKey(wd)) {
-                answer += map.get(wd);
+            if(map.containsKey(w4)){
+                answer+=map.get(w4);
             }
-            map.put(wa, map.getOrDefault(wa, 0) + 1);
+            map.put(w1,map.getOrDefault(w1,0)+1);
         }
-
+        
         return answer;
     }
 }
