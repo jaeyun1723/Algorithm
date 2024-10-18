1c4105639eee85290478f7b372484ddffc8541bc
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-10-18 15:46:41 +0900

    [level 3] Title: 부대복귀, Time: 192.56 ms, Memory: 174 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        int[] answer = new int[sources.length];
+        int[] dp = new int[n + 1];
+        ArrayList<Integer>[] list = new ArrayList[n + 1];
+        for (int i = 1; i < list.length; i++) {
+            list[i] = new ArrayList<>();
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int[] road : roads) {
+            list[road[0]].add(road[1]);
+            list[road[1]].add(road[0]);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        pq.add(new int[]{destination, 0});
+        dp[destination] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            if (dp[now[0]] < now[1]) {
+                continue;
+            }
+            for (int next : list[now[0]]) {
+                if (dp[next] > dp[now[0]] + 1) {
+                    dp[next] = dp[now[0]] + 1;
+                    pq.add(new int[]{next, dp[next]});
+                }
+            }
+        }
+
+        for (int i = 0; i < sources.length; i++) {
+            if (dp[sources[i]] == Integer.MAX_VALUE) {
+                answer[i] = -1;
+            } else {
+                answer[i] = dp[sources[i]];
+            }
+        }
+        return answer;
+    }
+}
