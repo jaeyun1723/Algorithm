2b74ef685c3f4185aea5cc5b5413841de4a2875c
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-10-21 17:39:14 +0900

    [Gold V] Title: 최소비용 구하기, Time: 380 ms, Memory: 50764 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        ArrayList<Schedule>[] list = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list[from].add(new Schedule(from, to, cost));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+        int[] dist = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.add(new int[]{start, 0});
+
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+//            if (dist[now[0]] < now[1]) {
+//                continue;
+//            }
+            if (now[0] == end) {
+//                System.out.println(dist[end]);
+                break;
+            }
+            if (!visited[now[0]]) {
+                visited[now[0]] = true;
+                for (int i = 0; i < list[now[0]].size(); i++) {
+                    Schedule s = list[now[0]].get(i);
+                    if (!visited[s.to] && dist[s.to] >= now[1] + s.cost) {
+                        dist[s.to] = now[1] + s.cost;
+                        pq.add(new int[]{s.to, now[1] + s.cost});
+                    }
+                }
+            }
+        }
+
+        System.out.println(dist[end]);
+    }
+
+    public static class Schedule {
+
+        int from;
+        int to;
+        int cost;
+
+        public Schedule(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+}
