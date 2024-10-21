a315472b01f4933dc6a6c9be048430f6a1be0e37
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-10-21 18:24:43 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,27 +1,26 @@
 import java.util.*;
 
 class Solution {
+
     public int solution(int storey) {
-        int answer=0;
-        while(storey!=0){
+        int answer = 0;
+        while (storey != 0) {
             int num = storey % 10;
-            storey/=10;
-            if(num==5){
-                if(storey%10>=5){
-                    answer+=10-num;
-                    storey++;
+            if (num == 5) {
+                if ((storey / 10) % 10 >= 5) { // 55 65 75
+                    storey += 5;
+                } else {
+                    storey -= 5;
                 }
-                else
-                    answer+=num;
+                answer += 5;
+            } else if (num > 5) {
+                storey += 10 - num;
+                answer += 10 - num;
+            } else {
+                storey -= num;
+                answer += num;
             }
-            else if(num > 5) {
-                answer += 10-num;
-                storey++;
-               
-            }
-            else {answer+=num;
-           
-                 }
+            storey /= 10;
         }
         return answer;
     }
