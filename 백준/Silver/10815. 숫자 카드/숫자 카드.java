3a3976a3e520b9bb806b7e4717f514eb0a800f2a
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-10-22 15:42:17 +0900

    [Silver V] Title: 숫자 카드, Time: 2768 ms, Memory: 166728 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        Map<Integer, Integer> map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, num);
+        }
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (map.containsKey(num))
+                System.out.print(1+" ");
+            else System.out.print(0+" ");
+        }
+        System.out.println();
+    }
+}
