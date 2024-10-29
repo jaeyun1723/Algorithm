b79c73bf953df5b39125259456169b1d33c1043c
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2024-10-29 16:22:44 +0900

    [Gold IV] Title: 단어 수학, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,56 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 
-class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-
-        String[] str = new String[N];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            str[i] = st.nextToken();
-        }
-
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            int digit = 1;
-            for (int j = str[i].length() - 1; j >= 0; j--) {
-                char c = str[i].charAt(j);
-                map.put(c, map.getOrDefault(c, 0) + digit);
-                digit *= 10;
-            }
-        }
-
-        List<Character> keys = new ArrayList<>(map.keySet());
-
-        keys.sort(new Comparator<Character>() {
-            @Override
-            public int compare(Character o1, Character o2) {
-                return map.get(o2) - map.get(o1);
-            }
-        });
-        int num = 9;
-        for (char key : keys) {
-            map.put(key, num--);
-        }
-        long answer = 0L;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < str[i].length(); j++) {
-                char c = str[i].charAt(j);
-                if (c >= 'A' && c <= 'Z') {
-                    str[i] = str[i].replace(c, (char) (map.get(c) + '0'));
-                }
-            }
-            answer += Long.parseLong(str[i]);
-        }
-        // 맨 앞자리부터 가장 많은 것 : 9
-        System.out.println(answer);
-    }
+public class Main {
+	static int N;
+	static int [] arr = new int[26];
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < str.length(); j++) {
+				char c = str.charAt(j);
+				arr[c-'A'] += (int)Math.pow(10, str.length() - 1 - j);
+			}
+		}
+		
+		Arrays.sort(arr);
+		
+		int num = 9;
+		int turn = 25;
+		int ans = 0;
+		while(arr[turn] != 0) {
+			ans += arr[turn]*num;
+			turn--;
+			num--;
+		}
+		
+		System.out.print(ans);
+	}
 }
