716699b0e88c9c4d2098ffdd517d95cd2707f37f
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-04-09 09:55:06 +0900

    [Gold V] Title: 토마토, Time: 776 ms, Memory: 116156 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,60 +6,69 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+
 class Main {
 
-    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, 1, -1};
+    static int M, N;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static boolean isRipe(int[][] arr) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        int[][] tomato = new int[N][M];
-        boolean[][] isDone = new boolean[N][M];
+        int[][] arr = new int[N][M];
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> Integer.compare(o1[2], o2[2])));
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                tomato[i][j] = Integer.parseInt(st.nextToken());
-                if (tomato[i][j] == 1) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
                     pq.add(new int[]{i, j, 0});
-                    tomato[i][j] = -2;
-                    isDone[i][j] = true;
                 }
-            }
-        }
-
-        while (!pq.isEmpty()) {
-            int[] now = pq.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M || isDone[nx][ny]) {
-                    continue;
-                }
-                if (tomato[nx][ny] == -1) {
-                    continue;
-                }
-                isDone[nx][ny] = true;
-                tomato[nx][ny] = now[2] + 1;
-                pq.add(new int[]{nx, ny, now[2] + 1});
             }
         }
         int answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (tomato[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
+        boolean[][] isVisit = new boolean[N][M];
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            answer = Math.max(answer, now[2]);
+            if (!isVisit[now[0]][now[1]]) {
+                isVisit[now[0]][now[1]] = true;
+                for (int i = 0; i < 4; i++) {
+                    int nx = now[0] + dx[i];
+                    int ny = now[1] + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= M || isVisit[nx][ny]) {
+                        continue;
+                    }
+                    if (arr[nx][ny] == 0) {
+                        arr[nx][ny] = now[2] + 1;
+                        pq.add(new int[]{nx, ny, now[2] + 1});
+                    }
                 }
-                answer = Math.max(answer, tomato[i][j]);
             }
         }
-        System.out.println(answer);
+        System.out.println(isRipe(arr) ? answer : -1);
+
+
     }
+
+
 }
