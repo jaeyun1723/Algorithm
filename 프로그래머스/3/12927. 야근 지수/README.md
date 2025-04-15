6ba1919978ed4252f148d2bc63096961c763283a
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-04-15 18:03:08 +0900

    [level 3] Title: 야근 지수, Time: 298.86 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=java) 
 
 ### 성능 요약
 
-메모리: 67.7 MB, 시간: 106.25 ms
+메모리: 89.7 MB, 시간: 298.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 11:50:23
+2025년 04월 15일 18:03:18
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
 class Solution {
+
     public long solution(int n, int[] works) {
         long answer = 0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>(new Comparator<Integer>(){
-            public int compare(Integer o1, Integer o2){
-                return o2-o1;
-            }
-        });
-        for(int i=0;i<works.length;i++)
-            pq.offer(works[i]);
-        while(n-->0){
-            int work=pq.poll();
-            if(work==0) break;
-            pq.offer(--work);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < works.length; i++) {
+            pq.add(works[i]);
         }
-        
-        while(!pq.isEmpty())
-            answer+=Math.pow(pq.poll(),2);
+
+        while (n-- > 0 && !pq.isEmpty()) {
+            int k = pq.poll();
+            if (k != 0) {
+                pq.add(--k);
+            }
+        }
+        int size = pq.size();
+        for (int i = 0; i < size; i++) {
+            if (!pq.isEmpty()) {
+                answer += (long) Math.pow(pq.poll(), 2);
+            }
+        }
         return answer;
     }
 }
