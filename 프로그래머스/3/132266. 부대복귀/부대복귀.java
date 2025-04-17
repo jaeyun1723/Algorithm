0d0039a5f43a110958d1296d75fae0bf3141e941
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-04-17 13:28:52 +0900

    [level 3] Title: 부대복귀, Time: 175.57 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,45 +2,50 @@ import java.util.*;
 
 class Solution {
 
-    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        int[] answer = new int[sources.length];
-        int[] dp = new int[n + 1];
-        boolean[] visited = new boolean[n + 1];
-        ArrayList<Integer>[] list = new ArrayList[n + 1];
-        for (int i = 1; i < list.length; i++) {
-            list[i] = new ArrayList<>();
-            dp[i] = Integer.MAX_VALUE;
-        }
-        for (int[] road : roads) {
-            list[road[0]].add(road[1]);
-            list[road[1]].add(road[0]);
-        }
+    static ArrayList<Integer>[] list;
+    static final int INF = 100001;
+    static int[] area;
+
+    public static void dijkstra(int start) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-        pq.add(new int[]{destination, 0});
-        dp[destination] = 0;
+        boolean[] isVisited = new boolean[area.length];
+        pq.add(new int[]{start, 0});
+        area[start] = 0;
 
         while (!pq.isEmpty()) {
             int[] now = pq.poll();
-            if (dp[now[0]] < now[1]) {
+            if (area[now[0]] < now[1]) {
                 continue;
             }
-            if (!visited[now[0]]) {
-                visited[now[0]] = true;
+            if (!isVisited[now[0]]) {
+                isVisited[now[0]] = true;
                 for (int next : list[now[0]]) {
-                    if (!visited[next] && dp[next] > dp[now[0]] + 1) {
-                        dp[next] = dp[now[0]] + 1;
-                        pq.add(new int[]{next, dp[next]});
+                    if (area[next] > now[1] + 1) {
+                        area[next] = now[1] + 1;
+                        pq.add(new int[]{next, now[1] + 1});
                     }
                 }
             }
         }
+    }
 
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        list = new ArrayList[n + 1];
+        area = new int[n + 1];
+        Arrays.fill(area, INF);
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<Integer>();
+        }
+
+        for (int[] road : roads) {
+            list[road[0]].add(road[1]);
+            list[road[1]].add(road[0]);
+        }
+
+        dijkstra(destination);
+        int[] answer = new int[sources.length];
         for (int i = 0; i < sources.length; i++) {
-            if (dp[sources[i]] == Integer.MAX_VALUE) {
-                answer[i] = -1;
-            } else {
-                answer[i] = dp[sources[i]];
-            }
+            answer[i] = (area[sources[i]] == INF) ? -1 : area[sources[i]];
         }
         return answer;
     }
