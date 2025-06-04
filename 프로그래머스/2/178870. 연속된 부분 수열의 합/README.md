1f2e8c9b12f07e16bd1370059ceba24a046bf795
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-06-04 12:26:09 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 15.01 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
 
 ### 성능 요약
 
-메모리: 138 MB, 시간: 42.85 ms
+메모리: 141 MB, 시간: 15.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 15:43:54
+2025년 06월 04일 12:26:06
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,22 +1,23 @@
 class Solution {
 
     public int[] solution(int[] sequence, int k) {
-        int[] answer = {0, sequence.length - 1};
+        int[] answer = new int[2];
+        answer[1] = sequence.length - 1;
         int sum = 0;
-        int end = 0;
+        int left = 0;
+
         for (int i = 0; i < sequence.length; i++) {
-            while (sum < k && end < sequence.length) {
-                sum += sequence[end++];
+            sum += sequence[i];
+            while (sum > k && i < sequence.length) {
+                sum -= sequence[left++];
             }
             if (sum == k) {
-                if (end - 1 - i < answer[1] - answer[0]) {
-                    answer[0] = i;
-                    answer[1] = end - 1;
+                if (i - left < answer[1] - answer[0]) {
+                    answer[0] = left;
+                    answer[1] = i;
                 }
             }
-            sum -= sequence[i];
         }
-
         return answer;
     }
 }
