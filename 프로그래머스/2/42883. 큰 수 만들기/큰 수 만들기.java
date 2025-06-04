572e784024f6e3b809e852f314c736e725b50090
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-06-04 22:22:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 9563.23 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,23 +1,22 @@
-import java.util.*;
-
 class Solution {
 
     public String solution(String number, int k) {
-        String answer = "";
-        
-        StringBuilder sb=new StringBuilder();
-        int start=0;
-        for(int i=0;i<number.length()-k;i++){
-            char max=0;
-            for(int j=start;j<=i+k;j++){
-                if(max<number.charAt(j)){
-                    max=number.charAt(j);
-                    start=j+1;
+        StringBuilder answer = new StringBuilder();
+        int idx = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < number.length() - k; i++) {
+            char max = '0';
+
+            for (int j = idx; j <= i + k; j++) {
+                if (max < number.charAt(j)) {
+                    max = number.charAt(j);
+                    idx = j + 1;
                 }
             }
-            sb.append(max);
+
+            answer.append(max);
         }
-            
-        return answer=sb.toString();
+        return answer.toString();
     }
 }
