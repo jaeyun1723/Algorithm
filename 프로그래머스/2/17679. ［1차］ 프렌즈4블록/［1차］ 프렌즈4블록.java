c5c1d1703612ddfe1c8b03d34772931b67d955f0
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-06-04 23:36:32 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 33.16 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,76 +1,63 @@
 import java.util.*;
 
 class Solution {
-	int M, N;
-	int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-	char[][] blocks;
-	boolean[][] isBlocked;
 
-	public void arrange(ArrayList<int[]> arrayList) {
-		for (int i = 0; i < arrayList.size(); i++) {
-			int x = arrayList.get(i)[0];
-			int y = arrayList.get(i)[1];
-			blocks[x][y] = '.';
-			blocks[x + 1][y] = '.';
-			blocks[x][y + 1] = '.';
-			blocks[x + 1][y + 1] = '.';
-			isBlocked[x][y] = true;
-			isBlocked[x][y + 1] = true;
-			isBlocked[x + 1][y] = true;
-			isBlocked[x + 1][y + 1] = true;
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = M - 1; j >= 0; j--) {
-				if (blocks[j][i] == '.') {
-					int k = j - 1;
-					while (k >= 0 && blocks[k][i] == '.') {
-						k--;
-					}
-					if (k >= 0) {
-						blocks[j][i] = blocks[k][i];
-						blocks[k][i] = '.';
-					}
-				}
-			}
-		}
-	}
 
-	public ArrayList<int[]> check() {
-		ArrayList<int[]> list = new ArrayList<>();
-		for (int i = 0; i < M - 1; i++) {
-			for (int j = 0; j < N - 1; j++) {
-				if (blocks[i][j] != '.' && blocks[i][j] == blocks[i][j + 1] && blocks[i][j] == blocks[i + 1][j]
-					&& blocks[i][j] == blocks[i + 1][j + 1]) {
-					list.add(new int[] {i, j});
-				}
-			}
-		}
-		return list;
-	}
+    public boolean isMatch(int x, int y, char[][] map, boolean[][] block) {
+        if (map[x + 1][y] == map[x][y] && map[x][y + 1] == map[x][y]
+            && map[x + 1][y + 1] == map[x][y]) {
+            block[x][y] = block[x + 1][y] = block[x][y + 1] = block[x + 1][y + 1] = true;
+            return true;
+        }
+        return false;
+    }
 
-	public int solution(int m, int n, String[] board) {
-		int answer = 0;
-		blocks = new char[m][n];
-		isBlocked = new boolean[m][n];
-		for (int i = 0; i < m; i++) {
-			blocks[i] = board[i].toCharArray();
-		}
-		M = m;
-		N = n;
-		int r = 0;
-		int c = 0;
-		while (true) {
-			ArrayList<int[]> list = check();
-			if (list.isEmpty())
-				break;
-			arrange(list);
-		}
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				if (blocks[i][j]=='.')
-					answer++;
-			}
-		}
-		return answer;
-	}
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        char[][] map = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = board[i].charAt(j);
+            }
+        }
+        while (true) {
+            boolean[][] block = new boolean[m][n];
+            boolean flag = false;
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n - 1; j++) {
+                    if (map[i][j] != '0' && isMatch(i, j, map, block)) {
+                        flag = true;
+                    }
+                }
+            }
+            if (flag) {
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (block[i][j]) {
+                            map[i][j] = '0';
+                            answer++;
+                        }
+                    }
+                }
+                for (int i = 0; i < n; i++) {
+                    Stack<Character> stk = new Stack<>();
+                    for (int j = 0; j < m; j++) {
+                        if (map[j][i] != '0') {
+                            stk.push(map[j][i]);
+                        }
+                    }
+                    for (int j = m - 1; j >= 0; j--) {
+                        if (!stk.isEmpty()) {
+                            map[j][i] = stk.pop();
+                        } else {
+                            map[j][i] = '0';
+                        }
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
 }
