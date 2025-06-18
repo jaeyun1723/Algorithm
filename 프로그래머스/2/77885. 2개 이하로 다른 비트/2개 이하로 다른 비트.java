9f8f13f8d75dfa53547ff1a2dac9a587199301d6
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-06-18 11:36:50 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 94.30 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,10 +1,55 @@
+import java.util.*;
+
 class Solution {
-    public long[] solution(long[] numbers) {
-        long[] answer = numbers.clone();
-        for(int i=0;i<numbers.length;i++){
-            answer[i]++;
-            answer[i]+=(numbers[i]^answer[i])>>2;
+
+    public static long match(String num1, String num2, long cnt) {
+        int idx1 = num1.length() - 1;
+        int idx2 = num2.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        while (cnt > 2) {
+            if (num1.charAt(idx1) != num2.charAt(idx2)) {
+                sb.append(num1.charAt(idx1));
+                cnt--;
+            } else {
+                sb.append(num2.charAt(idx2));
+            }
+            idx1--;
+            idx2--;
         }
+        while(idx2>=0){
+            sb.append(num2.charAt(idx2--));
+        }
+        return Long.parseLong(sb.reverse().toString(),2);
+    }
+
+    public static long diff(String num1, String num2) {
+        if (num2.length() == num1.length() + 1) {
+            return num2.length();
+        } else {
+            int count = 0;
+            for (int i = num1.length() - 1; i >= 0; i--) {
+                if (num1.charAt(i) != num2.charAt(i)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            String num1 = Long.toBinaryString(numbers[i]);
+            String num2 = Long.toBinaryString(numbers[i] + 1);
+            long cnt = diff(num1, num2);
+            if (cnt <= 2) {
+                answer[i] = numbers[i] + 1;
+            } else {
+                answer[i] = match(num1, num2, cnt);
+            }
+        }
+
         return answer;
     }
 }
