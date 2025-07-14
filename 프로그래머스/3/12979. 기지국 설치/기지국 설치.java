35bc3b556b185664daa4571ea677c329be54630d
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-07-14 19:03:40 +0900

    [level 3] Title: 기지국 설치, Time: 1.03 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,28 +1,18 @@
 class Solution {
 
-    public int minStation(int start, int end, int w) {
-        int res = (end - start + 1) / (2 * w + 1);
-        if ((end - start + 1) % (2 * w + 1) != 0) {
-            res++;
-        }
-        return res;
-    }
-
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int start = 1;
-        int end = 0;
 
-        for (int i = 0; i < stations.length; i++) {
-            end = stations[i] - w - 1;
-            if (start <= end) {
-                answer += minStation(start, end, w);
+        int cur = 1;
+        int idx = 0;
+        while (cur <= n) {
+            if (idx >= stations.length || cur < stations[idx] - w) {
+                answer++;
+                cur = cur + 2 * w + 1;
+            } else {
+                cur = stations[idx] + w + 1;
+                idx++;
             }
-            start = stations[i] + w + 1;
-        }
-
-        if (start <= n) {
-            answer += minStation(start, n, w);
         }
 
         return answer;
