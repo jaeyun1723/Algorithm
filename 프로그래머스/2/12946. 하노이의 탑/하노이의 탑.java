4f57ea6be007a5862d824890341bb7d1ab0c3aa9
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-07-22 16:23:18 +0900

    [level 2] Title: 하노이의 탑, Time: 5.69 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
 
-    static ArrayList<int[]> list;
+    static ArrayList<int[]> seq;
 
-    public static void hanoi(int n, int start, int mid, int end) {
-        if (n == 0) {
-            return;
+    public void hanoi(int n, int start, int via, int end) {
+        if (n == 1) {
+            seq.add(new int[]{start, end});
+        } else {
+            hanoi(n - 1, start, end, via);
+            seq.add(new int[]{start, end});
+            hanoi(n - 1, via, start, end);
         }
-        hanoi(n - 1, start, end, mid);
-        // 1. 맨 마지막 원판을 end로 옮기기 위해선 나머지를 mid로 옮겨라.
-        list.add(new int[]{start, end});
-        // 2. 마지막 원판을 c로 이동
-        hanoi(n - 1, mid, start, end);
-        // 3. 나머지 원판(mid)을 c로 이동시켜라.
     }
 
     public int[][] solution(int n) {
-        list = new ArrayList<>();
+        int[][] answer = {};
+        seq = new ArrayList<>();
         hanoi(n, 1, 2, 3);
-        // hanoi(N, start, mid, end): start에서 end로 mid를 거쳐 총 N개의 원반을 운반할                                            때 각 이동 과정을 출력하라
-        int[][] answer = list.stream().toArray(int[][]::new);
-
+        answer = new int[seq.size()][2];
+        for (int i = 0; i < seq.size(); i++) {
+            answer[i] = seq.get(i);
+        }
         return answer;
     }
 }
