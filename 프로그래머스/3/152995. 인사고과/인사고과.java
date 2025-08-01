355cac262428eddac9e8b6d4142143f42c0f7882
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-08-01 16:26:26 +0900

    [level 3] Title: 인사고과, Time: 176.40 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -4,22 +4,37 @@ class Solution {
 
     public int solution(int[][] scores) {
         int answer = 0;
-        int[] wonho = scores[0];
-        // 근무 내림차순, (같은 경우) 동료 오름차순
-        Arrays.sort(scores, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
-        int maxScore = 0;
-        int rank = 1;
-        for (int[] score : scores) {
-            if (maxScore <= score[1]) {
-                maxScore = score[1];
-                if (score[0] + score[1] > wonho[0] + wonho[1]) {
-                    rank++;
+        int[] wan = scores[0];
+        if (scores.length == 1) {
+            return 1;
+        }
+        Arrays.sort(scores, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            }
+            return Integer.compare(o2[0], o1[0]);
+        });
+        int minScore = scores[0][1];
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i][1] < minScore) {
+                if (wan[0] == scores[i][0] && wan[1] == scores[i][1]) {
+                    return -1;
                 }
-            } else if (score.equals(wonho)) {
-                return -1;
+                scores[i][0] = -1;
+                scores[i][1] = -1;
+            } else {
+                minScore = scores[i][1];
             }
         }
 
-        return rank;
+        Arrays.sort(scores, (o1, o2) -> Integer.compare(o2[0] + o2[1], o1[0] + o1[1]));
+        for (int[] score : scores) {
+            if (wan[0] + wan[1] < score[0] + score[1]) {
+                answer++;
+            } else {
+                break;
+            }
+        }
+        return answer + 1;
     }
 }
