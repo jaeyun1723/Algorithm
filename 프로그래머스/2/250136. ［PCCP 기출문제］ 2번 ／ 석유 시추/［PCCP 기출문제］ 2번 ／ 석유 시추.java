a3e75f53c1b080451fc5f1c96d47ffc92bdbf5fa
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2025-09-17 07:22:07 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 77.13 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,55 +2,57 @@ import java.util.*;
 
 class Solution {
 
-    static int row, col;
-    static int[] oil;
+    static int n, m, idx = 0;
     static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+    static List<Integer> oils = new ArrayList<>();
 
-    public static void bfs(int x, int y, int[][] land, boolean[][] isVisit) {
-        int count = 1;
+    public void bfs(int r, int c, int[][] land, int[][] map, boolean[][] visited) {
         Deque<int[]> deq = new ArrayDeque<>();
-        HashSet<Integer> set = new HashSet<>();
-        deq.add(new int[]{x, y});
-        set.add(y);
-        isVisit[x][y] = true;
+        deq.offerLast(new int[]{r, c});
+        visited[r][c] = true;
+        int result = 1;
+        map[r][c] = idx;
         while (!deq.isEmpty()) {
-            int[] now = deq.poll();
-            set.add(now[1]);
+            int[] now = deq.pollFirst();
             for (int i = 0; i < 4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-                if (nx < 0 || nx >= row || ny < 0 || ny >= col || isVisit[nx][ny]) {
+                int x = now[0] + dx[i];
+                int y = now[1] + dy[i];
+                if (x < 0 || y < 0 || x >= n || y >= m || visited[x][y]) {
                     continue;
                 }
-                if (land[nx][ny] == 1) {
-                    isVisit[nx][ny] = true;
-                    count++;
-                    deq.add(new int[]{nx, ny});
+                if (land[x][y] == 1) {
+                    result++;
+                    visited[x][y] = true;
+                    deq.offerLast(new int[]{x, y});
+                    map[x][y] = idx;
                 }
             }
         }
-        for (int c : set) {
-            oil[c] += count;
-        }
+        oils.add(result);
+        idx++;
     }
 
     public int solution(int[][] land) {
         int answer = 0;
-        row = land.length;
-        col = land[0].length;
-        boolean[][] isVisit = new boolean[row][col];
-        oil = new int[col];
-        int label = 2;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (land[i][j] == 1 && !isVisit[i][j]) {
-                    bfs(i, j, land, isVisit);
+        n = land.length;
+        m = land[0].length;
+        int[][] map = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        for (int j = 0; j < m; j++) {
+            int sum = 0;
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < n; i++) {
+                if (land[i][j] == 1) {
+                    if (!visited[i][j]) {
+                        bfs(i, j, land, map, visited);
+                    }
+                    set.add(map[i][j]);
                 }
             }
-        }
-
-        for (int o : oil) {
-            answer = Math.max(answer, o);
+            for (int num : set) {
+                sum += oils.get(num);
+            }
+            answer = Math.max(answer, sum);
         }
         return answer;
     }
