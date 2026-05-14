119d451306b50ea6fc806f5e8b2f9b39c817ab6e
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-14 16:56:23 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 8.21 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,56 +1,65 @@
 import java.util.*;
 
 class Solution {
+    // 4:07
+
+    public File extractFile(int idx, String file) {
+        StringBuilder head = new StringBuilder();
+        StringBuilder number = new StringBuilder();
+        StringBuilder tail = new StringBuilder();
+        boolean flag = false;
+        for (int i = 0; i < file.length(); i++) {
+            char c = file.charAt(i);
+            if (Character.isDigit(c) && number.length() < 5) { // 숫자인 경우
+                flag = true;
+                number.append(c);
+            } else {
+                if (!flag) { // 숫자가 아니면서 아직 숫자 나온적 없음
+                    head.append(c);
+                } else { // 숫자가 아니면서 숫자 나온적 있음.
+                    tail.append(file.substring(i));
+                    break;
+                }
+            }
+        }
+        return new File(idx, head.toString(), number.toString(), tail.toString());
+    }
 
     public String[] solution(String[] files) {
         String[] answer = new String[files.length];
 
         ArrayList<File> list = new ArrayList<>();
         for (int i = 0; i < files.length; i++) {
-            String file = files[i];
-            StringBuilder head = new StringBuilder();
-            StringBuilder number = new StringBuilder();
-            StringBuilder tail = new StringBuilder();
-            boolean flag = false;
-            for (int j = 0; j < file.length(); j++) {
-                char c = file.charAt(j);
-                if (c >= '0' && c <= '9') {
-                    flag = true;
-                    number.append(c);
-                } else {
-                    if (flag) {
-                        tail.append(file.substring(j));
-                        break;
-                    } else {
-                        head.append(c);
-                    }
-                }
-            }
-            list.add(new File(i, head.toString(), number.toString(), tail.toString()));
+            list.add(extractFile(i, files[i]));
         }
 
-        list.sort(new Comparator<File>() {
-            public int compare(File o1, File o2) {
-                if (o1.head.toLowerCase().equals(o2.head.toLowerCase())) {
-                    if (Integer.parseInt(o1.number) == Integer.parseInt(o2.number)) {
-                        return Integer.compare(o1.idx, o2.idx);
-                    } else {
-                        return Integer.compare(Integer.parseInt(o1.number),
-                            Integer.parseInt(o2.number));
+        Comparator<File> comp = new Comparator<File>() {
+            public int compare(File f1, File f2) {
+                // HEAD
+                String h1 = f1.head.toLowerCase();
+                String h2 = f2.head.toLowerCase();
+                if (h1.equals(h2)) {
+                    // NUMBER
+                    int n1 = Integer.parseInt(f1.number);
+                    int n2 = Integer.parseInt(f2.number);
+                    if (n1 == n2) {
+                        // IDX
+                        return Integer.compare(f1.idx, f2.idx);
                     }
-                } else {
-                    return o1.head.toLowerCase().compareTo(o2.head.toLowerCase());
+                    return Integer.compare(n1, n2);
                 }
+                return h1.compareTo(h2);
             }
-        });
+        };
+        list.sort(comp);
 
-        for (int i = 0; i < answer.length; i++) {
+        for (int i = 0; i < list.size(); i++) {
             answer[i] = files[list.get(i).idx];
         }
         return answer;
     }
 
-    public class File {
+    public static class File {
 
         int idx;
         String head;
@@ -64,4 +73,5 @@ class Solution {
             this.tail = tail;
         }
     }
+
 }
