4aaafcb87d1af8290338613086db1c78656302e1
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-15 13:46:31 +0900

    [level 2] Title: 타겟 넘버, Time: 3.56 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,19 +2,19 @@ class Solution {
 
     static int answer;
 
-    public void subset(int idx, int[] numbers, int target, int sum) {
+    public void subset(int[] numbers, int target, int idx, int sum) {
         if (idx == numbers.length) {
             if (sum == target) {
                 answer++;
             }
             return;
         }
-        subset(idx + 1, numbers, target, sum + numbers[idx]);
-        subset(idx + 1, numbers, target, sum - numbers[idx]);
+        subset(numbers, target, idx + 1, sum + numbers[idx]);
+        subset(numbers, target, idx + 1, sum + (-1) * numbers[idx]);
     }
 
     public int solution(int[] numbers, int target) {
-        subset(0, numbers, target, 0);
+        subset(numbers, target, 0, 0);
         return answer;
     }
 }
