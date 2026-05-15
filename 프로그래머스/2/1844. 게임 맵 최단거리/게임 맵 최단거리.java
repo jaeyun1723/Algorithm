17d97897df65f65ecbbba12ca921ab842d71fde6
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-15 14:09:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.50 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,26 +1,35 @@
 import java.util.*;
-import java.awt.*;
+
 class Solution {
-    static int[] dx={0,0,-1,1},dy={-1,1,0,0};
+
+    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+
     public int solution(int[][] maps) {
-        int answer = 0;
-        boolean[][] isVisited= new boolean[maps.length][maps[0].length];
-        Deque<int[]> deq=new ArrayDeque<>();
-        deq.offer(new int[]{0,0,1});
-        isVisited[0][0]=true;
-        while(!deq.isEmpty()){
-            int[] now=deq.pollFirst();
-            if(now[0]==maps.length-1&&now[1]==maps[0].length-1)
-            {answer = now[2]; break;}
-            for(int i=0;i<4;i++){
-                int nextX=now[0]+dx[i], nextY=now[1]+dy[i];
-                if(nextX<0||nextY<0||nextX>=maps.length||nextY>=maps[0].length||isVisited[nextX][nextY]) continue;
-                if(maps[nextX][nextY]==0) continue;
-                isVisited[nextX][nextY]=true;
-                deq.offerLast(new int[]{nextX,nextY,now[2]+1});
+        int answer = -1;
+        int n = maps.length;
+        int m = maps[0].length;
+        Deque<int[]> deq = new ArrayDeque<>();
+        boolean[][] isVisited = new boolean[n][m];
+        deq.offerLast(new int[]{0, 0, 1}); // x,y,sum 
+        isVisited[0][0] = true;
+        while (!deq.isEmpty()) {
+            int[] now = deq.pollFirst();
+            if (now[0] == n - 1 && now[1] == m - 1) {
+                answer = (answer == -1) ? now[2] : Math.min(answer, now[2]);
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    int nx = now[0] + dx[i];
+                    int ny = now[1] + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m || isVisited[nx][ny]) {
+                        continue;
+                    }
+                    if (maps[nx][ny] == 1) {
+                        isVisited[nx][ny] = true;
+                        deq.offerLast(new int[]{nx, ny, now[2] + 1});
+                    }
+                }
             }
         }
-        if(answer==0) answer=-1;
         return answer;
     }
 }
