62b3331466a3a2607aa56b9f7eabd7a47927adec
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-16 20:39:11 +0900

    [level 2] Title: 배달, Time: 2.98 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,63 +2,64 @@ import java.util.*;
 
 class Solution {
 
-    static ArrayList<Road>[] list;
-
-    public int dijkstra(int N, int K) {
-        int[] dp = new int[N + 1];
-        Arrays.fill(dp, 10_000_000);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1[1],o2[1]));
+    public int dijkstra(ArrayList<Road>[] list, int N, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> (Integer.compare(p1[1], p2[1])));
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        boolean[] isVisited = new boolean[N + 1];
+        dist[1] = 0;
         pq.add(new int[]{1, 0});
-        dp[1] = 0;
-        boolean[] visited = new boolean[N + 1];
         while (!pq.isEmpty()) {
-            int[] now = pq.poll(); // 현재 정점, 최소 거리
-            if (dp[now[0]] < now[1]) {
+            int[] now = pq.poll();
+            if (now[1] > dist[now[0]]) {
+                continue; // 이거 왜 필요한지 모르겠음.
+            }
+            if (now[1] > K) {
                 continue;
             }
-            if (!visited[now[0]]) {
-                visited[now[0]] = true;
+            if (!isVisited[now[0]]) { // boolean이 필요한가? pq이기 때문에 boolean이필요한가? pq로 인해서 한번도착한 곳은 무조건 가까운 것이 보장되니까?
+                isVisited[now[0]] = true;
                 for (int i = 0; i < list[now[0]].size(); i++) {
-                    Road next = list[now[0]].get(i);
-                    if (dp[next.dest] > dp[now[0]] + next.cost) {
-                        dp[next.dest] = dp[now[0]] + next.cost;
-                        pq.add(new int[]{next.dest, dp[now[0]] + next.cost});
+                    Road next = list[now[0]].get(i); // 여기서 visited봐야하나?
+                    if (dist[next.dest] > now[1] + next.len) { // > 괄호가 맞나? = 은 생각안해도되나
+                        dist[next.dest] = now[1] + next.len;
+                        pq.add(new int[]{next.dest, now[1] + next.len});
                     }
                 }
             }
         }
         int count = 0;
         for (int i = 1; i <= N; i++) {
-            if (dp[i] <= K) {
+            if (dist[i] <= K) {
                 count++;
             }
         }
         return count;
+
     }
 
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        list = new ArrayList[N + 1];
+        ArrayList<Road>[] list = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<Road>();
+            list[i] = new ArrayList<>();
         }
+
         for (int[] r : road) {
             list[r[0]].add(new Road(r[1], r[2]));
             list[r[1]].add(new Road(r[0], r[2]));
         }
-
-        return answer = dijkstra(N, K);
+        return answer = dijkstra(list, N, K);
     }
 
-    public static class Road {
+    public class Road {
 
         int dest;
-        int cost;
+        int len;
 
-        public Road(int dest, int cost) {
+        public Road(int dest, int len) {
             this.dest = dest;
-            this.cost = cost;
+            this.len = len;
         }
-
     }
 }
