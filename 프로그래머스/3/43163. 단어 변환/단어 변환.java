48dad51065634e78bd29295a6cca9d2cc1cf7d64
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-16 19:41:57 +0900

    [level 3] Title: 단어 변환, Time: 0.07 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,33 +1,44 @@
+import java.util.*;
+
 class Solution {
 
-  static int answer = Integer.MAX_VALUE;
+    static int answer;
 
-  public int difference(String w1, String w2) {
-    int result = 0;
-    for (int i = 0; i < w1.length(); i++) {
-      if (w1.charAt(i) != w2.charAt(i)) {
-        result++;
-      }
+    public void dfs(String begin, String target, String[] words, boolean[] isVisited, int count) {
+        if (begin.equals(target)) {
+            answer = Math.min(answer, count);
+            return;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (!isVisited[i] && canChange(begin, words[i])) {
+                isVisited[i] = true;
+                dfs(words[i], target, words, isVisited, count + 1);
+                isVisited[i] = false;
+            }
+        }
     }
-    return result;
-  }
 
-  public void dfs(String start, String end, String[] words, boolean[] isChange, int count) {
-    if (start.equals(end)) {
-      answer = Math.min(answer, count);
-      return;
+    public boolean canChange(String a, String b) {
+        int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        if (count == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    for (int i = 0; i < words.length; i++) {
-      if (!isChange[i] && difference(start, words[i]) == 1) {
-        isChange[i] = true;
-        dfs(words[i], end, words, isChange, count + 1);
-        isChange[i] = false;
-      }
-    }
-  }
 
-  public int solution(String begin, String target, String[] words) {
-    dfs(begin, target, words, new boolean[words.length], 0);
-    return answer == Integer.MAX_VALUE ? 0 : answer;
-  }
+    public int solution(String begin, String target, String[] words) {
+        answer = 1000;
+        boolean[] isVisited = new boolean[words.length];
+        dfs(begin, target, words, isVisited, 0);
+        return answer == 1000 ? 0 : answer;
+    }
 }
