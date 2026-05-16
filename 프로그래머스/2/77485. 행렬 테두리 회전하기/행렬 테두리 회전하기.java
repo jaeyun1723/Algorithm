71e2e9b18dee2ba4df7e6c08e0c44f21259120a6
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-16 22:04:34 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 5.97 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,53 +1,53 @@
-import java.util.*;
-
 class Solution {
 
-    public int rotate(int[] query, int[][] arr) {
-        // x1, x2 y1, y2 -> x1, y1 y1+1 ~ x1,y2
-        int x1 = query[0], y1 = query[1], x2 = query[2], y2 = query[3];
-        int prev = arr[x1][y1];
-        int min = prev;
-        for (int i = y1 + 1; i <= y2; i++) {
-            int tmp = arr[x1][i];
-            arr[x1][i] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
+    static int[][] map;
+
+    public int rotate(int[] query) {
+        int x1 = query[0];
+        int y1 = query[1];
+        int x2 = query[2];
+        int y2 = query[3];
+
+        // 시작점의 값 임시 저장
+        int temp = map[x1][y1];
+
+        int min = temp;
+        for (int i = x1; i < x2; i++) {
+            map[i][y1] = map[i + 1][y1];
+            min = Math.min(min, map[i][y1]);
         }
-        for (int i = x1 + 1; i <= x2; i++) {
-            int tmp = arr[i][y2];
-            arr[i][y2] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
+
+        for (int j = y1; j < y2; j++) {
+            map[x2][j] = map[x2][j + 1];
+            min = Math.min(min, map[x2][j]);
         }
-        for (int i = y2 - 1; i >= y1; i--) {
-            int tmp = arr[x2][i];
-            arr[x2][i] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
+        for (int i = x2; i > x1; i--) {
+            map[i][y2] = map[i - 1][y2];
+            min = Math.min(min, map[i][y2]);
         }
-        for (int i = x2 - 1; i >= x1; i--) {
-            int tmp = arr[i][y1];
-            arr[i][y1] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
+        for (int j = y2; j > y1; j--) {
+            map[x1][j] = map[x1][j - 1];
+            min = Math.min(min, map[x1][j]);
         }
+        map[x1][y1 + 1] = temp;
+
         return min;
     }
 
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
-        int[][] arr = new int[rows + 1][columns + 1];
+        map = new int[rows + 1][columns + 1];
         int num = 1;
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= columns; j++) {
-                arr[i][j] = num++;
+                map[i][j] = num++;
             }
         }
 
         for (int i = 0; i < queries.length; i++) {
-            answer[i] = rotate(queries[i], arr);
+            answer[i] = rotate(queries[i]);
         }
-        
+
         return answer;
     }
 }
