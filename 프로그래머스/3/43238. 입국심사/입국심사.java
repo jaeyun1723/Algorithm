76f74553519eb839a02c5bc9ce1b9a7295ec88d1
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-16 21:29:27 +0900

    [level 3] Title: 입국심사, Time: 18.85 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,24 +2,46 @@ import java.util.*;
 
 class Solution {
 
+    public boolean canEvaluate(int n, int[] times, long limit) {
+        long count = 0;
+        for (int time : times) {
+            count += (limit / time);
+            if (count >= n) {
+                return true;
+            }
+        }
+        return count >= n;
+    }
+
     public long solution(int n, int[] times) {
         long answer = 0;
-        Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length - 1] * (long)n;
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long cnt = 0;
-            for (int i = 0; i < times.length; i++) {
-                cnt += mid / times[i];
-            }
-            if (cnt >= n) {
-                right = mid - 1;
+        long low = 1;
+        long high = 0;
+        for (int i = 0; i < times.length; i++) {
+            high = Math.max(high, times[i]);
+        }
+        high *= n;
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (canEvaluate(n, times, mid)) {
                 answer = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
         return answer;
+    }
+
+    public class Inspector {
+
+        int cur;
+        int full;
+
+        public Inspector(int cur, int full) {
+            this.cur = cur;
+            this.full = full;
+        }
     }
 }
