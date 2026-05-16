96fc43069e47228c1831d2f3ce0991c664ce4cf9
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2026-05-16 19:20:49 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,45 +2,25 @@ import java.util.*;
 
 class Solution {
 
-    static int[] parent;
-
-    public int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-
-    public void union(int x, int y) {
-        int p1 = find(x);
-        int p2 = find(y);
-        if (p1 > p2) {
-            parent[p1] = p2;
-        } else {
-            parent[p2] = p1;
+    public void dfs(int n, int[][] computers, boolean[] isVisited, int cur) {
+        for (int i = 0; i < n; i++) {
+            if (computers[cur][i] == 1 && !isVisited[i]) {
+                isVisited[i] = true;
+                dfs(n, computers, isVisited, i);
+            }
         }
     }
 
     public int solution(int n, int[][] computers) {
-        parent = new int[n];
+        boolean[] isVisited = new boolean[n];
+        int answer = 0;
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (computers[i][j] == 1 && i != j) {
-                    if (find(i) != find(j)) {
-                        union(i, j);
-                    }
-                }
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                dfs(n, computers, isVisited, i);
+                answer++;
             }
         }
-
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            set.add(find(i));
-        }
-        return set.size();
+        return answer;
     }
 }
